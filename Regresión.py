5d7bf4b0501c162200b531171c34043e0bbba2ca
RodrigoT-P <a01703521@tex.mx>
2024-05-07 10:25:39 -0600

    REGRESION EXCEL

---
+++ Regresión.py
@@ -0,0 +1,9 @@
+
+
+import webbrowser
+
+# URL que deseas abrir en el navegador
+url = "https://docs.google.com/spreadsheets/d/11qbkQrJMXB-bRnpWUTxiYFfCNUm0zXfp/edit?usp=sharing&ouid=104735921156825710662&rtpof=true&sd=true"
+
+# Abrir la URL en el navegador predeterminado del sistema
+webbrowser.open(url)
